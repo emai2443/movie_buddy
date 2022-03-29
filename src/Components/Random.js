dcb3238b6ce9e1fd17b062fc1f34bcbfb99189e8
Eric Mai <ericmai10314@gmail.com>
2022-03-29 16:31:37 -0400

    navbar

--- src/Components/Random.js
+++ src/Components/Random.js
@@ -1,31 +1,26 @@
-import React,{ useEffect, useState } from 'react'
-import { Movie } from './Movie';
+import React, { useEffect, useState } from "react";
+import { Movie } from "./Movie";
 
-const LATEST_API = `https://api.themoviedb.org/3/movie/latest?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
-
+// https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1
 
 export const Random = () => {
   const [movies, setMovies] = useState([]);
   var latest_id = 955389;
 
-  let random = Math.floor(Math.random() * (latest_id - 1) + 1)
-  var RANDOM_API = `https://api.themoviedb.org/3/movie/${random}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US`
+  let random = Math.floor(Math.random() * (latest_id - 1) + 1);
+  var RANDOM_API = `https://api.themoviedb.org/3/movie/${random}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&adult=false`;
 
   //implementation:
   //movie details are console logged
   //couldn't figure out how to get movie vote_average
   //
   //want to get random movie -> check if movie's rating is >5
-                          // -> if not -> get a new random movie
-
-
-
+  // -> if not -> get a new random movie
 
   // function randomMovie() {
 
   //   check()
   // }
-
 
   // function check() {
 
@@ -37,35 +32,33 @@ export const Random = () => {
       .then((data) => {
         setMovies(data);
         // console.log(data.results);
-        console.log(data)
+        console.log(data);
       });
   }, []);
 
+  // const getMovies = (url) => {
+  //   fetch(url).then(res => res.json()).then(data => {
+  //     if(data.results.length !== 0) { //If there are movies with selected genre, show them
+  //       setMovies(data.results);
+  //       let v = document.getElementById('emptyResult')
+  //       v.innerHTML = ``;
+  //     } else { //If no movies with selected genre, show message "No Results"
+  //       setMovies(data.results);
+  //       let v = document.getElementById('emptyResult')
+  //       v.innerHTML = `<h1 class="no-result">No Results</h1>`;
+  //       //console.log('empty!');
+  //       // setMovies([])
+  //     }
 
+  //   })
+  // }
 
-
-// const getMovies = (url) => {
-//   fetch(url).then(res => res.json()).then(data => {
-//     if(data.results.length !== 0) { //If there are movies with selected genre, show them
-//       setMovies(data.results);
-//       let v = document.getElementById('emptyResult')
-//       v.innerHTML = ``;
-//     } else { //If no movies with selected genre, show message "No Results"
-//       setMovies(data.results);
-//       let v = document.getElementById('emptyResult')
-//       v.innerHTML = `<h1 class="no-result">No Results</h1>`;
-//       //console.log('empty!');
-//       // setMovies([])
-//     }
-
-//   })
-// }
-
-
-  return (<>
+  return (
+    <>
       <div>Random webpage</div>
       <div className="movie-container">
         <Movie {...movies} />
       </div>
-    </>)
+    </>
+  );
 };
