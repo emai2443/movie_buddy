dc4489c0712be28fa8d8eb47ee32fa3c159d7b31
Eric Mai <ericmai10314@gmail.com>
2022-03-30 01:34:10 -0400

    tweak

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,20 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AccountContext } from "./Account";
-
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { authenticate } = useContext(AccountContext);
+  //   const HomeButton = () => {
+  //     let history = useNavigate();
+  //   };
+  const navigate = useNavigate();
+  const logIn = () => {
+    navigate("/user");
+    //if
+    //else to check authentication
+  };
   const onSubmit = (event) => {
     event.preventDefault();
 
@@ -12,6 +22,7 @@ const Login = () => {
       .then((data) => {
         console.log("Logged in!", data);
       })
+      .then(logIn)
       .catch((err) => {
         console.error("Failed to login", err);
       });
@@ -30,7 +41,9 @@ const Login = () => {
           value={password}
           onChange={(event) => setPassword(event.target.value)}
         ></input>
-        <button type="submit">Login</button>
+        <button type="submit" onSuceess={logIn}>
+          Go home
+        </button>
       </form>
     </div>
   );
