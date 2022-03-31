e81e791c662d8340c951917ef99a259b9209de80
Edmund-Adewu <Edmund.Adewu39@myhunter.cuny.edu>
2022-03-30 22:52:09 -0400

    Centered registration & login fields.

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -5,10 +5,15 @@ import Status from "./Status";
 
 const SignIn = () => {
   return (
+    <div style={{
+      position: 'absolute', left: '50%', top: '50%',
+        transform: 'translate(-50%, -50%)'
+    }}>
     <Account>
       <Status />
       <Login />
     </Account>
+    </div>
   );
 };
 export default SignIn;
