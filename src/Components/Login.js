2cec237497436515cfbfd23c453dae9717e9ef0d
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-05-01 22:31:04 -0400

    failedpassword

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -9,6 +9,8 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import { deepPurple } from "@mui/material/colors";
 
+import Modal from '@mui/material/Modal';
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -27,10 +29,43 @@ const Login = () => {
       .then(logIn)
       .catch((err) => {
         console.error("Failed to login", err);
+        handleOpen()
       });
   };
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    backgroundColor: 'white',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
+    <Modal
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+  >
+    <Box sx={style}>
+      <Typography id="modal-modal-title" variant="h6" component="h2"sx={{ mt: 2 , color: "black"}}>
+        Text in a modal
+      </Typography>
+      <Typography id="modal-modal-description" sx={{ mt: 2 , color: "black"}}>
+        Wrng pass my guy
+      </Typography>
+    </Box>
+  </Modal>
       <Container
         component="main"
         maxWidth="sm"
