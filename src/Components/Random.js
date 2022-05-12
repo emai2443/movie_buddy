06329470a9cf986c69e6d6c8e7582327537aa572
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2022-05-12 17:58:50 -0400

    amplify

--- src/Components/Random.js
+++ src/Components/Random.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Movie } from "./Movie";
 import { RandomMovie } from "./RandomMovie";
 import { Account } from "./Account";
 import Status from "./Status";
 
 export const Random = () => {
-  const [movies, setMovies] = useState([]);
-  var latest_id = 600000;
 
-  let random = Math.floor(Math.random() * (latest_id - 1) + 500000);
-  var RANDOM_API = `https://api.themoviedb.org/3/movie/${random}?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&adult=false`;
+
+
+  const [movies, setMovies] = useState([]);
 
   let page = Math.floor(Math.random() * 10 + 1);
   let TEST_API = `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=${page}&sort_by=popularity.desc&vote_average.gte=7&vote_average.lte=10`;
@@ -40,10 +38,10 @@ export const Random = () => {
       .then((res) => res.json())
       .then((data) => {
         setMovies(data.results);
-        // console.log(data.results);
-        // console.log(data.results);
       });
   };
+
+
 
   return (
     <>
@@ -51,15 +49,7 @@ export const Random = () => {
         <Status />
         <div></div>
         <div className="movie-container">
-          {/* {movies.length > 0 &&
-          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-        ; */}
-          {/* {movies.map((movie) => <Movie key={movie.id}{...movie}/>)} */}
           <RandomMovie key={num1} {...movies[num1]} />
-          {/* <Movie key={num2} {...movies[num2]} />
-        <Movie key={num3} {...movies[num3]} />
-        <Movie key={num4} {...movies[num4]} />
-        <Movie key={num5} {...movies[num5]} /> */}
         </div>
         <div className="refreshButton">
           <button className="applyButton" onClick={() => reroll()}>
